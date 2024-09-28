1c35ebfb77d775cf51e175258a54c2751990dd69
rouk5555 <153026365+rouk5555@users.noreply.github.com>
2024-09-27 22:30:09 -0400

    Changed main and added Log-Parser

--- main2.ps1
+++ main2.ps1
@@ -1,6 +1,10 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Log-Parser.ps1)
 
 clear
 
 $result = Get-Logs -Page 'index.html' -Code '404' -Browser 'Chrome'
 $result
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
